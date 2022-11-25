731bf09f34a05374bb968b31fe8e7bac45399573
Ali Hassan <ah759886@gmail.com>
2022-11-26 00:33:16 +0500

    new

--- client/src/helper.js
+++ client/src/helper.js
@@ -1,3 +1,3 @@
-export const ServerUrl='https://cors-anywhere.herokuapp.com/https://parkingspace1.herokuapp.com/api/v1/'
+export const ServerUrl='https://parkingspace1.herokuapp.com/api/v1/'
 
 export const LocalUrl='http://localhost:8000/api/v1/'
